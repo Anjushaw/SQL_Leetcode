1295d95cba7a5ec6c17290003b4d5295eda55bfd
Anju Shaw <72430762+Anjushaw@users.noreply.github.com>
2025-01-09 11:07:22 +0530

    1148. Article Views I.sql

--- 1148. Article Views I.sql
+++ 1148. Article Views I.sql
@@ -47,6 +47,13 @@ Output:
 +------+
 | id   |
 +------+
+
+
+ # Write your MySQL query statement below
+select distinct author_id as id
+from views
+where author_id=viewer_id
+order by 1;
 | 4    |
 | 7    |
 +------+
